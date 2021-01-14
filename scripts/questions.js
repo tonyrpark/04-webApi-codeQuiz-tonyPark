bc8c630e9b06f54fa3cb3dad68d79198dcbbfc97
tonyrpark <tonyrpark@gmail.com>
2021-01-14 10:24:19 -0800

    Finished structure of index.html without CSS, created structure of questions.js file

---
+++ scripts/questions.js
@@ -0,0 +1,73 @@
+var questionBank = [
+
+// Question 1
+{
+    question: "What do you need to write to change font style in CSS?",
+    selection: ["All the below", "font-family", "font-weight", "font color"],
+    answer: "font-family"
+},
+
+// Question 2
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 3 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 4 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 5 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 6 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 7 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 8 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 9 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+},
+
+// Question 10 
+{
+    question: "",
+    selection: [""],
+    answer: ""
+}
+
+];
