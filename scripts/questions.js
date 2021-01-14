4a43e980221231a3b07095b23f1ac64c63e82145
tonyrpark <tonyrpark@gmail.com>
2021-01-14 11:28:15 -0800

    finished questions file

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,73 +1,105 @@
 var questionBank = [
-
-// Question 1
-{
+  // Question 1
+  {
     question: "What do you need to write to change font style in CSS?",
     selection: ["All the below", "font-family", "font-weight", "font color"],
-    answer: "font-family"
-},
+    answer: "font-family",
+  },
 
-// Question 2
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 2
+  {
+    question:
+      "Where in an HTML document is the correct place to refer to an internal style sheet?",
+    selection: [
+      "in the <body> section",
+      "in the <head> section",
+      "at the end of the document",
+      "None of the above",
+    ],
+    answer: "in the <head> section",
+  },
 
-// Question 3 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 3
+  {
+    question:
+      "What is the correct HTML for referring to an external CSS stylesheet?",
+    selection: [
+      "<link rel='stylesheet' type='text/css' href='mystyle.css'>",
+      "<stylesheet>mystyle.css</stylesheet>",
+      "<stylesheet>mystyle</stylesheet>",
+    ],
+    answer: "<link rel='stylesheet' type='text/css' href='mystyle.css'>",
+  },
 
-// Question 4 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 4
+  {
+    question: "What does a <div> tag do?",
+    selection: [
+      "The <div> tag defines a division or a section in an HTML document.",
+      "The <div> tag is used to group block-elements to format them with CSS.",
+      "The <div> tag divides the nation into absolute peril.",
+      "A and B",
+    ],
+    answer: "A and B",
+  },
 
-// Question 5 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 5
+  {
+    question: "What does HTML stand for?",
+    selection: [
+      "Hot Tamales Make Love",
+      "Hypertext Markup Language",
+      "Hyper Tense Marker Literals",
+      "Hypertext Mobile Language",
+    ],
+    answer: "Hypertext Markup Language",
+  },
 
-// Question 6 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 6
+  {
+    question: "What is the correct format for a 'comment' in HTML?",
+    selection: [
+      "/*  comment  */",
+      "<$-- comment -->",
+      "<!-- comment -->",
+      " #comment",
+    ],
+    answer: "<!-- comment -->",
+  },
 
-// Question 7 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 7
+  {
+    question:
+      "What index number is Kansas City Chiefs? var nflTeams = ['Los Angeles Rams', 'Chicago Bears', 'Philadelphia Eagles', 'Kansas City Chiefs']",
+    selection: ["0", "1", "2", "3"],
+    answer: "3",
+  },
 
-// Question 8 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 8
+  {
+    question: "What function gets you a random math number?",
+    selection: [
+      "Math.floor();",
+      "Math.round();",
+      "Math.split();",
+      "Math.random();",
+    ],
+    answer: "Math.random();",
+  },
 
-// Question 9 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-},
+  // Question 9
+  {
+    question:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    selection: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
 
-// Question 10 
-{
-    question: "",
-    selection: [""],
-    answer: ""
-}
-
+  // Question 10
+  {
+    question:
+      "Which of the following are examples of JavaScript variable data types?",
+    selection: ["number", "null", "Boolean", "All of the above"],
+    answer: "All of the above",
+  },
 ];
